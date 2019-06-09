7a8fe5ec28e3ad3e26647ac4a30eb4a792ca3063
Joshua Bennett <joshuabennett@utexas.edu>
2019-06-08 23:50:10 -0500

    fixed an error where validation errors on one player was giving validation errors on another player

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -1,38 +1,38 @@
 <template>
     <div class="columns">
         <div class="column player-column">
-            <input class='input' name="name" v-validate="'required|alpha_num'" type="text" :placeholder="'Player ' + index" v-model='value.name' 
-            :class="{'is-danger' : errors.has('name')}" autocomplete="off">
+            <input class='input' :name="index +'name'" v-validate="'required|alpha_num'" type="text" :placeholder="'Player ' + index" v-model='value.name' 
+            :class="{'is-danger' : errors.has(index + 'name')}" autocomplete="off">
         </div>
         <div class="column">
-            <input name='rating' class='input' type="text" v-validate="'required|numeric'" placeholder='0' v-model='value.rating'
-            :class="{'is-danger' : errors.has('rating')}" autocomplete="off">
-            <span v-show="errors.has('rating')" class='is-size-7 valerror'> Requires a number </span>
+            <input :name="index + 'rating'" class='input' type="text" v-validate="'required|numeric'" placeholder='0' v-model='value.rating'
+            :class="{'is-danger' : errors.has(index + 'rating')}" autocomplete="off">
+            <span v-show="errors.has(index + 'rating')" class='is-size-7 valerror'> Requires a number </span>
         </div>
         <div class="column">
-            <input name='awards' class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.awards'
-            :class="{'is-danger' : errors.has('awards')}" autocomplete="off">
-            <span v-show="errors.has('awards')" class='is-size-7 valerror'> Requires a number </span>
+            <input :name="index + 'awards'" class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.awards'
+            :class="{'is-danger' : errors.has(index + 'awards')}" autocomplete="off">
+            <span v-show="errors.has(index + 'awards')" class='is-size-7 valerror'> Requires a number </span>
         </div>
         <div class="column">
-            <input name='milestones' class='input' type="text" placeholder='0' v-validate="'required|numeric'"  v-model='value.milestones'
-            :class="{'is-danger' : errors.has('milestones')}" autocomplete="off">
-            <span v-show="errors.has('milestones')" class='is-size-7 valerror'> Requires a number </span>
+            <input :name="index + 'milestones'" class='input' type="text" placeholder='0' v-validate="'required|numeric'"  v-model='value.milestones'
+            :class="{'is-danger' : errors.has(index + 'milestones')}" autocomplete="off">
+            <span v-show="errors.has(index + 'milestones')" class='is-size-7 valerror'> Requires a number </span>
         </div>
         <div class="column">
-            <input name='cities' class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.cities' 
-            :class="{'is-danger' : errors.has('cities')}" autocomplete="off">
-            <span v-show="errors.has('cities')" class='is-size-7 valerror'> Requires a number </span>
+            <input :name="index + 'cities'" class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.cities' 
+            :class="{'is-danger' : errors.has(index + 'cities')}" autocomplete="off">
+            <span v-show="errors.has(index + 'cities')" class='is-size-7 valerror'> Requires a number </span>
         </div>
         <div class="column">
-            <input name='greenery' class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.greenery' 
-            :class="{'is-danger' : errors.has('greenery')}" autocomplete="off">
-            <span v-show="errors.has('greenery')" class='is-size-7 valerror'> Requires a number </span>
+            <input :name="index + 'greenery'" class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.greenery' 
+            :class="{'is-danger' : errors.has(index + 'greenery')}" autocomplete="off">
+            <span v-show="errors.has(index + 'greenery')" class='is-size-7 valerror'> Requires a number </span>
         </div>
         <div class="column">
-            <input  name='cards' class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.cards' 
-            :class="{'is-danger' : errors.has('cards')}" autocomplete="off">
-            <span v-show="errors.has('cards')" class='is-size-7 valerror'> Requires a number </span>
+            <input  :name="index + 'cards'" class='input' type="text" placeholder='0' v-validate="'required|numeric'" v-model='value.cards' 
+            :class="{'is-danger' : errors.has(index + 'cards')}" autocomplete="off">
+            <span v-show="errors.has(index + 'cards')" class='is-size-7 valerror'> Requires a number </span>
         </div>
     </div>
 </template>
